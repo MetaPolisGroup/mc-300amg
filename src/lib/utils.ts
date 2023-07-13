8110208186029cfca64bf61741cea28e06369a01
chainman2209 <chainsawman2209@gmail.com>
2023-07-13 15:35:50 +0700

    feat: add field bet

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,26 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...input: ClassValue[]) => {
   return twMerge(clsx(input));
 };
+
+/**
+ * * format input only number
+ * @param event InputHTML
+ * @returns
+ */
+export const formatInputField = (event: any) => {
+  const digitPeriodRegExp = new RegExp("\\d|\\.");
+  if (
+    event.ctrlKey || // (A)
+    event.altKey || // (A)
+    typeof event.key !== "string" || // (B)
+    event.key.length !== 1
+  ) {
+    // (C)
+    return;
+  }
+
+  if (!digitPeriodRegExp.test(event.key)) {
+    console.log(1);
+    event.preventDefault();
+  }
+};
