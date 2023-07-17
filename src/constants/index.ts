23633e0e278232bb4813acd18227765d0d0f3209
nbi67 <nbi67@NYNEO>
2023-07-17 09:41:58 +0700

    feat: connect wallet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,12 @@
 import { AMOUNT_MINIMUM_REQUIRED } from "./amountMinimum";
+import { ChainType, chain } from "./chain";
+import provider from "./provider";
+
+const CURRENT_NETWORK = ChainType.SEPOLIA;
 
 const CURRENCY_UNIT = "BNB";
 export const CONSTANTS = {
   AMOUNT_REQUIRED: AMOUNT_MINIMUM_REQUIRED[CURRENCY_UNIT].VALUE,
+  CHAIN: chain[CURRENT_NETWORK],
+  PROVIDER: provider(CURRENT_NETWORK),
 };
