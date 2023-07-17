f2a6993973ce21fa54f96717f8f223bd4b2de435
nbi67 <nbi67@NYNEO>
2023-07-17 16:25:08 +0700

    chore: config contract address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,19 @@
 import { AMOUNT_MINIMUM_REQUIRED } from "./amountMinimum";
 import { ChainType, chain } from "./chain";
 import provider from "./provider";
+import { predictionAddr } from "./address/index";
+import { predictionABI } from "./abi/index";
 
-const CURRENT_NETWORK = ChainType.SEPOLIA;
+const CURRENT_NETWORK = ChainType.BSCTESTNET;
 
 const CURRENCY_UNIT = "BNB";
 export const CONSTANTS = {
+  ADDRESS: {
+    PREDICTION: predictionAddr[CURRENT_NETWORK],
+  },
+  ABI: {
+    PREDICTION: predictionABI[CURRENT_NETWORK],
+  },
   AMOUNT_REQUIRED: AMOUNT_MINIMUM_REQUIRED[CURRENCY_UNIT].VALUE,
   CHAIN: chain[CURRENT_NETWORK],
   PROVIDER: provider(CURRENT_NETWORK),
