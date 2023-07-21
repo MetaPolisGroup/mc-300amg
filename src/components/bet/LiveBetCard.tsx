af93b89678c8d8a77f990d5c295410b55bc2ca13
chainman2209 <chainsawman2209@gmail.com>
2023-07-21 16:55:09 +0700

    feat: add leaderboard ui

--- src/components/bet/LiveBetCard.tsx
+++ src/components/bet/LiveBetCard.tsx
@@ -7,7 +7,11 @@ interface ILiveBetCardProps {
 }
 
 const LiveBetCard: React.FC<ILiveBetCardProps> = ({ currentRound }) => {
-  const dataBetted: any = {};
+  const dataBetted: any = {
+    id: "",
+    status: "UP",
+    value: "",
+  };
   return (
     <div
       className={`w-full flex justify-center items-center relative transition-transform duration-700 preverve-3d`}
