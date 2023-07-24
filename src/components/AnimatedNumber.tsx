17bbdb1492892856b280b3a5c1cba5d1bba4ab65
chainman2209 <chainsawman2209@gmail.com>
2023-07-24 22:29:51 +0700

    feat: add animated number

---
+++ src/components/AnimatedNumber.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+interface IAnimatedNumberProps {
+  startNumber: number;
+  endNumber: number;
+}
+
+const AnimatedNumber: React.FC<IAnimatedNumberProps> = ({
+  startNumber,
+  endNumber,
+}) => {
+  const { number } = useSpring({
+    from: { number: startNumber },
+    number: endNumber,
+    delay: 200,
+  });
+  return <animated.div>{number.to((n) => n.toFixed(4))}</animated.div>;
+};
+
+export default AnimatedNumber;
