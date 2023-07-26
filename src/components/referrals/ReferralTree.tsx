6ef9bb0dea94b30ab821fbe35e5202ab8f3130ae
chainman2209 <chainsawman2209@gmail.com>
2023-07-26 17:01:39 +0700

    fix: not apply css in mobile

---
+++ src/components/referrals/ReferralTree.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const ReferralTree = () => {
+  return (
+    <div className="text-white  p-3 border-2 border-[--colors-secondary] rounded-xl">
+      <div className="flex justify-between items-center">
+        <span className="text-xl md:text-3xl">Referral Tree</span>
+        {/* <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            style={{ padding: "0 60px 0 10px" }}
+            className="bg-[#1b1b1b] rounded-xl h-[50px] md:w-[500px] w-[150px]"
+          />
+          <div className="bg-[#252525] rounded absolute p-[12px] right-[10px] top-[5px]">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 512 512"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+            </svg>
+          </div>
+        </div> */}
+      </div>
+      {/* <div className="flex gap-2 my-2 flex-wrap">
+        <div className="flex items-center gap-2  border p-1 bg-slate-700   rounded">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            focusable="false"
+            className="chakra-icon css-bfrb7x"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6zM432 480H80a31 31 0 01-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75 1.94 10.73-.68 21.34-7.18 29.11A31 31 0 01432 480z"></path>
+          </svg>
+          <span className="flex items-center text-sm">Direct Sponsor</span>
+        </div>
+        <div className="flex items-center gap-2  border p-1 bg-slate-700   rounded">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            focusable="false"
+            className="chakra-icon css-1h8y4d7"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6zM432 480H80a31 31 0 01-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75 1.94 10.73-.68 21.34-7.18 29.11A31 31 0 01432 480z"></path>
+          </svg>
+          <span className="flex items-center text-sm">With Referral(s)</span>
+        </div>
+        <div className="flex items-center gap-2 border p-1 bg-slate-700 rounded">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            focusable="false"
+            className="chakra-icon css-bo6s3p"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6zM432 480H80a31 31 0 01-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75 1.94 10.73-.68 21.34-7.18 29.11A31 31 0 01432 480z"></path>
+          </svg>
+          <span className="flex items-center text-sm">Without referral(s)</span>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default ReferralTree;
