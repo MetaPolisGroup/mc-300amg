e8a155de38ea299165a75b139b8612344c08c293
quocbao <ba0kieu952000@gmail.com>
2023-07-26 17:09:54 +0700

    feat: fill data to chart

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -14,6 +14,9 @@ import { CanvasRenderer } from "echarts/renderers";
 import getDataFileredByOnSnapshot from "@/helpers/getDataFilteredByOnSnapshot";
 import { DocumentData } from "firebase/firestore";
 
+import dayjs from "dayjs";
+import HeaderChart from "./HeaderChart";
+
 echarts.use([
   GridComponent,
   ToolboxComponent,
@@ -31,6 +34,12 @@ type EChartsOption = echarts.ComposeOption<
 
 const Chart: React.FC = () => {
   const [chartData, setChartData] = React.useState<DocumentData[]>([]);
+
+  const dataHeaderRef = React.useRef<{ time: string; price: number | null }>({
+    time: "",
+    price: null,
+  });
+
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
@@ -49,7 +58,21 @@ const Chart: React.FC = () => {
   useEffect(() => {
     var chartDom = document.getElementById("main")!;
     var myChart = echarts.init(chartDom);
-    console.log({ chartData });
+
+    let time: any = [];
+    let price: any = [];
+    let min = 100000;
+    let max = 0;
+
+    chartData?.map((chart) => {
+      const _price = chart.price / 10 ** 8;
+      min = _price < min ? _price : min;
+      max = _price > max ? _price : max;
+
+      time = [...time, dayjs(chart.created_at * 1000).format("h:mm A")];
+      price = [...price, _price];
+    });
+
     const option: EChartsOption = {
       tooltip: {
         trigger: "axis",
@@ -59,28 +82,63 @@ const Chart: React.FC = () => {
             backgroundColor: "#6a7985",
           },
         },
+        formatter: (params: any) => {
+          dataHeaderRef.current = {
+            price: params?.[0]?.data.toFixed(4),
+            time: params?.[0]?.axisValue,
+          };
+
+          return `${params?.[0]?.axisValue}<br><b>${params?.[0]?.data.toFixed(
+            4
+          )} BNB/USD</b>`;
+        },
       },
       xAxis: {
         type: "category",
         boundaryGap: false,
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        data: time,
+        axisLabel: {
+          color: "#9a6aff",
+          fontWeight: 500,
+        },
       },
       yAxis: {
+        show: true,
         type: "value",
-        show: false,
+        position: "right",
+        min: min,
+        max: max,
+        splitLine: {
+          show: false,
+        },
+        axisLabel: {
+          color: "#9a6aff",
+          fontWeight: 500,
+        },
       },
       series: [
         {
-          data: [820, 932, 901, 934, 1290, 1330, 2000],
+          data: price,
           type: "line",
           areaStyle: {},
+          itemStyle: { borderColor: "#ffc700" },
+          lineStyle: { color: "#19c0cc" },
         },
       ],
+      color: ["#53dee9"],
     };
     option && myChart.setOption(option);
   }, [chartData]);
 
-  return <div id="main" style={{ width: "100%", height: "300px" }}></div>;
+  return (
+    <div>
+      <HeaderChart
+        time={dataHeaderRef.current.time}
+        price={dataHeaderRef.current.price ?? 0}
+      />
+      <div id="main" style={{ width: "100%", height: "300px" }}></div>
+    </div>
+  );
 };
 
 export default Chart;
