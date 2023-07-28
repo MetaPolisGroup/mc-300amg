5118493aed082bf77cbee824b30a7c4445c39467
Duy Khuong <khuongtran227@gmail.com>
2023-07-28 17:02:18 +0700

    tree

--- src/components/referrals/ReferralTree.tsx
+++ src/components/referrals/ReferralTree.tsx
@@ -1,97 +1,114 @@
 "use client";
-import { getEllipsisTxt } from "@/utils/formmater-address";
+import getDataFileredByOnSnapshot from "@/helpers/getDataFilteredByOnSnapshot";
 import React from "react";
 import { useAccount } from "wagmi";
 import Button from "../ui/Button";
-const data = ["a"];
+
 const ReferralTree = () => {
   const { address } = useAccount();
-  const treeData = [
-    {
-      id: "A",
-      children: [
-        {
-          id: "B",
-          parent: "A",
-          children: [
-            { id: "B1", parent: "B", children: [] },
-            { id: "B2", parent: "B", children: [] },
-            { id: "B3", parent: "B", children: [] },
-          ],
-        },
-        {
-          id: "C",
-          parent: "A",
-          children: [
-            { id: "C1", parent: "C", children: [] },
-            { id: "C2", parent: "C", children: [] },
-            { id: "C3", parent: "C", children: [] },
-          ],
-        },
-        {
-          id: "D",
-          parent: "A",
-          children: [
-            { id: "D1", parent: "D", children: [] },
-            { id: "D2", parent: "D", children: [] },
-            { id: "D3", parent: "D", children: [] },
-          ],
-        },
-      ],
-    },
-  ];
-
   const [visibleNodes, setVisibleNodes] = React.useState<any[]>([]);
+  const [child, setChild] = React.useState<string[]>([]);
+  const [data, setData] = React.useState<string[]>([]);
 
-  const handleToggleNode = (nodeId: number) => {
-    if (visibleNodes.includes(nodeId)) {
-      setVisibleNodes((prevVisibleNodes) =>
-        prevVisibleNodes.filter((node) => node !== nodeId)
-      );
-    } else {
-      setVisibleNodes((prevVisibleNodes) => [...prevVisibleNodes, nodeId]);
-    }
+  const handleGetData = (id: string) => {
+    return getDataFileredByOnSnapshot(
+      "users",
+      [["user_tree_commissions", "array-contains", id]],
+      (docs) => {
+        setData(
+          docs
+            .filter((i) => i.user_tree_belong[0] === id)
+            .map((i) => i) as unknown as string[]
+        );
+      }
+    );
   };
-  const renderTreeNode = (node: any) => {
-    console.log(node.parent && visibleNodes.includes(node.parent));
+
+  const handleToggleNode = (nodeId: string, isSubTree: boolean) => {
+    console.log(nodeId);
+
+    getDataFileredByOnSnapshot(
+      "users",
+      [["user_tree_commissions", "array-contains", nodeId]],
+      (docs) => {
+        if (isSubTree)
+          setChild(
+            docs
+              .filter((i) => i.user_tree_belong[0] === nodeId)
+              .map((i) => i) as unknown as string[]
+          );
+        else
+          setVisibleNodes(
+            docs
+              .filter((i) => i.user_tree_belong[0] === nodeId)
+              .map((i) => i) as unknown as string[]
+          );
+      }
+    );
+  };
+
+  const renderTreeNode = (
+    node: any,
+    isSubTree: boolean,
+    isDisnable?: boolean
+  ) => {
     return (
       <div key={node.id} className="flex relative">
         <div
           className={`w-12 h-12 rounded flex items-center justify-center border-2 border-black cursor-pointer m-2 ${
             visibleNodes.includes(node.id) ? " border border-green-400" : ""
           }`}
-          onClick={() => handleToggleNode(node.id)}
+          onClick={() => handleToggleNode(node.id, isSubTree)}
         >
-          {node.id}
+          {node.id && <ButtonItem text={`${node.id}`} disnable={isDisnable} />}
         </div>
-        {visibleNodes.includes(node.id) && node.children.length > 0 && (
-          <div className="ml-4 flex flex-col">
-            {node.children.map((childNode: any) => renderTreeNode(childNode))}
-          </div>
-        )}
       </div>
     );
   };
+
   return (
     <div className="text-white  p-3 border-2 border-[--colors-secondary] rounded-xl">
       <div>
         <div className="text-xl mb-8 md:text-3xl">Referral Tree</div>
-        {/* <ButtonItem text="F1" /> */}
-        <div>{treeData.map((rootNode) => renderTreeNode(rootNode))}</div>
+        <div className="grid justify-self-center grid-cols-4 gap-5">
+          <div className="mt-[10px]">
+            <ButtonItem text="Root" handle={() => handleGetData(address!)} />
+          </div>
+          {data && (
+            <div className="block mx-auto px-5 h-[500px] overflow-y-scroll ">
+              {data.map((rootNode) => renderTreeNode(rootNode, false))}
+            </div>
+          )}
+          {visibleNodes && (
+            <div className="ml-4 block mx-auto px-5 h-[500px] overflow-y-scroll  ">
+              {visibleNodes.map((childNode: any) =>
+                renderTreeNode(childNode, true)
+              )}
+            </div>
+          )}
+          {child && (
+            <div className="ml-4 block mx-auto px-5 h-[500px] overflow-y-scroll  ">
+              {child.map((childNode: any) =>
+                renderTreeNode(childNode, true, true)
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-const ButtonItem: React.FC<{ text: string; handle?: () => void }> = ({
-  handle,
-  text,
-}) => (
+const ButtonItem: React.FC<{
+  text: string;
+  handle?: () => void;
+  disnable?: boolean;
+}> = ({ handle, text, disnable }) => (
   <Button
+    disabled={disnable}
     onClick={() => handle?.()}
-    className="bg-gradient-to-r from-[--colors-lightBlueLeft] to-[--colors-lightBlueRight] text-center py-2 rounded-2xl"
+    className="bg-gradient-to-r from-[#00CEEA] to-[#28C38B] text-center py-2 rounded-2xl w-[264px] h-[60px]"
   >
-    {/* {getEllipsisTxt(address!, 6)} */}
     {text}
   </Button>
 );
