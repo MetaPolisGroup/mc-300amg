c94cf6acbc4636149937c11eada4fd25d1180130
quocbao <ba0kieu952000@gmail.com>
2023-07-28 15:50:25 +0700

    feat: setting chart, create file index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,152 +1,118 @@
 "use client";
-import { useEffect, useState } from "react";
-
+import CoinCurrency from "@/components/CoinCurrency";
+import CountDown from "@/components/CountDown";
+import SubNav, { MODE } from "@/components/SubNav";
+import Card from "@/components/bet-bo/Card";
+import ClaimModal from "@/components/bet-bo/ClaimModal";
+import Chart from "@/components/chart/Chart";
+import DrawerHistory from "@/components/drawer-history/DrawerHistory";
+import Popup, { PopupRef } from "@/components/ui/Modal";
 import clsx from "clsx";
-import { useAccount } from "wagmi";
-import toast from "react-hot-toast";
-import Input from "@/components/ui/Input";
-import { Icons } from "@/components/Icons";
+import React, { createRef, useEffect, useState } from "react";
 
-import userApi from "@/services/user-api";
-import Button from "@/components/ui/Button";
-import Modal from "@/components/ui/Modal/Modal";
-import { useSearchParams } from "next/navigation";
+const Prediction = () => {
+  const [modeSubNavMobile, setModeSubNavMobile] = useState<string>(MODE.CHART);
 
-import SliderListGame from "@/components/sliderListGame";
-import SliderBannerGame from "@/components/sliderBannerGame";
-import getDataFileredByOnSnapshot from "@/helpers/getDataFilteredByOnSnapshot";
+  const collectWinningsRef = createRef<PopupRef>();
+  const [collectWinning, setCollectWinning] = useState<number>();
 
-export default function Home() {
-  const { isConnected, address } = useAccount();
-  const [showUserNickname, setShowUserNickname] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [nicknameValue, setNicknameValue] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isScreenMobile, setIsScreenMobile] = useState(false);
 
-  const searchParams = useSearchParams();
-
-  const recommendId = searchParams.get("id");
+  const isShowDrawer = modeSubNavMobile === MODE.HISTORY;
 
   useEffect(() => {
-    if (isConnected && address) {
-      getDataFileredByOnSnapshot(
-        "users",
-        [["user_address", "==", address]],
-        (docs) => {
-          console.log(docs);
-          if (docs?.[0]?.nickname === "") {
-            return setShowUserNickname(true);
-          }
-          if (docs.length === 0) {
-            return setShowUserNickname(true);
-          }
-          setShowUserNickname(false);
-        }
-      );
-    }
-  }, [isConnected, address]);
+    setIsScreenMobile(1024 > screen.width);
+  }, []);
 
-  const changeNicknameValueHandler = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const {
-      target: { value },
-    } = e;
-    setNicknameValue(value);
-  };
+  useEffect(() => {
+    let timeSet = setTimeout(() => {
+      if (!isScreenMobile) return;
 
-  const submitNicknameHandler = async () => {
-    setIsLoading(true);
-    if (nicknameValue.trim().length === 0) {
-      setErrorMessage("Nick name is not empty!");
-      setIsLoading(false);
-      return;
+      return setModeSubNavMobile(MODE.CARD);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeSet);
+    };
+  }, [isScreenMobile]);
+
+  const handlerToggleCollectWinning = (status: boolean, round: number) => {
+    setCollectWinning(round);
+
+    if (status === true) {
+      return collectWinningsRef.current?.open();
     }
-    try {
-      const response = await userApi.nickname({
-        user_address: address!,
-        nickname: nicknameValue,
-        recommend_id: recommendId ? recommendId : "",
-      });
-      if (response) {
-        setIsLoading(false);
-        setShowUserNickname(false);
-        console.log(response);
-        toast.custom((t) => (
-          <div
-            className={`${
-              t.visible ? "animate-enter" : "animate-leave"
-            } max-w-md w-full bg-[--colors-backgroundAlt] shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-          >
-            <div className="flex bg-[--colors-success] p-4 rounded-l-lg">
-              <Icons.CheckCircle className="text-[--colors-white]" />
-            </div>
-            <div className="flex-1 w-0 p-2">
-              <div className="flex items-start">
-                <div className="flex-shrink-0 pt-0.5"></div>
-                <div className="ml-3 flex-1">
-                  <p className="text-sm font-medium text-[--colors-text]">
-                    Success!
-                  </p>
-                  <p className="mt-1 text-sm text-[--colors-text]">
-                    Your nickname has been updated!
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="flex">
-              <button
-                onClick={() => toast.dismiss(t.id)}
-                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-start justify-end text-sm font-medium focus:outline-none"
-              >
-                <Icons.X className="text-[--colors-primary]" />
-              </button>
-            </div>
-          </div>
-        ));
-      }
-    } catch (error) {
-      setIsLoading(false);
-      console.log(error);
-    }
+    return collectWinningsRef.current?.close();
   };
 
   return (
     <main className="bg-gradient-to-r from-[--colors-violetAlt1] to-[--colors-violetAlt2] overflow-hidden">
-      <SliderListGame />
-
-      <SliderBannerGame />
-
-      {/* <Modal
-        show={showUserNickname}
-        title="Username"
-        width={500}
-        styleContent={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "5px",
-          backgroundColor: "transparent !important",
-          backgroundImage:
-            "linear-gradient(100%, var(--colors-lightBlueLeft), var(--colors-lightBlueRight))",
-        }}
-      >
-        <div>
-          <p>Enter your nickname:</p>
-          <Input
-            className="bg-white border-[--colors-success] border-2 rounded-2xl text-black px-2 py-4"
-            onChange={changeNicknameValueHandler}
-            onFocus={() => setErrorMessage("")}
-          />
-          <span className="text-sm text-red-500">{errorMessage}</span>
-        </div>
-        <Button
-          variant="success"
-          onClick={submitNicknameHandler}
-          isLoading={isLoading}
+      <div className="flex overflow-hidden">
+        <div
+          className={clsx(
+            "overflow-hidden",
+            isShowDrawer ? "w-[0px] lg:w-[calc(100%-385px)]" : "w-[100%]"
+          )}
         >
-          Confirm
-        </Button>
-      </Modal> */}
+          <div className="text-[--colors-failure] p-4">
+            <div className="flex flex-nowrap justify-between">
+              <CoinCurrency />
+              <CountDown
+                title="5m"
+                onAction={{
+                  setIsShowDrawer: () => {
+                    setModeSubNavMobile(MODE.HISTORY);
+                  },
+                }}
+              />
+            </div>
+          </div>
+
+          <div
+            className={clsx(
+              modeSubNavMobile !== MODE.CARD && isScreenMobile && "hidden"
+            )}
+          >
+            <Card />
+          </div>
+
+          <div
+            className={clsx(
+              modeSubNavMobile !== MODE.CHART && isScreenMobile && "hidden"
+            )}
+          >
+            <Chart />
+          </div>
+        </div>
+        <DrawerHistory
+          open={isShowDrawer}
+          onClose={() => setModeSubNavMobile(MODE.CARD)}
+          onCollect={handlerToggleCollectWinning}
+        />
+      </div>
+      <SubNav modeMobile={modeSubNavMobile} onAction={setModeSubNavMobile} />
+
+      <Popup
+        ref={collectWinningsRef}
+        width={300}
+        footer={false}
+        closable
+        title="Collect Winnnings"
+        styleContent={{
+          background: "var(--colors-backgroundAlt)",
+          color: "var(--colors-text)",
+        }}
+        content={
+          <ClaimModal
+            winningRound={collectWinning}
+            onCancel={() => {
+              handlerToggleCollectWinning(false, 0);
+            }}
+          />
+        }
+      />
     </main>
   );
-}
+};
+
+export default Prediction;
