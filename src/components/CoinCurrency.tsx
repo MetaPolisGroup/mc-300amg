00b5da38aa1fff569d1ebbb91433ee99c57e1ca8
chainman2209 <chainsawman2209@gmail.com>
2023-08-04 14:50:20 +0700

    fix: copied incorect link

--- src/components/CoinCurrency.tsx
+++ src/components/CoinCurrency.tsx
@@ -7,7 +7,7 @@ const CoinCurrency: React.FC = () => {
     <div className="bg-[--colors-backgroundAlt] p-2 px-3 rounded-3xl text-[--colors-text] flexcursor-pointer   justify-center flex items-center rounded-br-3xl gap-2">
       <Image src="/svgs/bnb-logo.svg" alt="" width={40} height={40} />
       <div className="text-base font-semibold md:text-lg">
-        <span>BNBUSD</span>
+        <span>BNB/USD</span>
       </div>
     </div>
   );
