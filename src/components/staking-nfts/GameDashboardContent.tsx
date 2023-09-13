0a913c6dcaf6af5377c0fbeeedb8df64c5dc583a
quocbao <ba0kieu952000@gmail.com>
2023-09-13 16:10:34 +0700

    feat: layout mobile page staking nfts

--- src/components/staking-nfts/GameDashboardContent.tsx
+++ src/components/staking-nfts/GameDashboardContent.tsx
@@ -2,10 +2,45 @@
 import React, { useState } from "react";
 import Button from "../ui/Button";
 import LayoutItemContent from "./LayoutItemContent";
+import ListNFTs from "./ListNFTs";
 
-const GameDashboardContent = () => {
+interface IGameDashboardContent {
+  isShowListNFTsMobile: boolean;
+}
+
+const GameDashboardContent: React.FC<IGameDashboardContent> = ({
+  isShowListNFTsMobile,
+}) => {
   const [reward, setReward] = useState<string>("0.0");
   const claimReward = () => {};
+
+  const renderContent = () => {
+    if (isShowListNFTsMobile)
+      return (
+        <div className="block md:hidden">
+          <ListNFTs />
+        </div>
+      );
+
+    return (
+      <div className="flex flex-wrap max-h-[656px] overflow-y-auto pt-3 gap-2">
+        {Array(3)
+          .fill(0)
+          .map((item, index) => (
+            <LayoutItemContent
+              key={index}
+              // image={item.image}
+              // description={item.description}
+              // title={item.title}
+              // dna={item.dna}
+              // date={item.date}
+              // tokenId={item.tokenId}
+            />
+          ))}
+      </div>
+    );
+  };
+
   return (
     <div className="md:px-24 md:grow md:pt-10">
       <div className="hidden md:block text-4xl text-[--colors-textSub] font-bold">
@@ -31,21 +66,7 @@ const GameDashboardContent = () => {
           </div>
         </div>
 
-        <div className="flex flex-wrap max-h-[656px] overflow-y-auto pt-3 gap-2">
-          {Array(3)
-            .fill(0)
-            .map((item, index) => (
-              <LayoutItemContent
-                key={index}
-                // image={item.image}
-                // description={item.description}
-                // title={item.title}
-                // dna={item.dna}
-                // date={item.date}
-                // tokenId={item.tokenId}
-              />
-            ))}
-        </div>
+        {renderContent()}
       </div>
     </div>
   );
